188fe491f11481dda71e30ad85e12993ae23c013
Киреев Михаил <105597940+Wassssssuppppppp@users.noreply.github.com>
2022-11-24 18:43:04 +0300

    Add files via upload

---
+++ Classes/Post.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EfCoreTest1
+{
+    public class Post
+    {
+        [Key]
+        public int PostionCode { get; set; }
+
+        public string TitleOfThePosition { get; set; }
+
+    }
+}
---
+++ Classes/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EfCoreTest1
+{
+    public class Product
+    {
+        [Key]
+        public int ProductCode { get; set; }
+
+        public string Title { get; set; }
+
+        public decimal PricePerPiece { get; set; }
+        public int? ZakazOrderCode { get; set; }
+        public Zakaz? Zakaz { get; set; }
+    }
+}
