1cd90a3ab6266b7236ce9da84e1b0072405e2a9c
Киреев Михаил <105597940+Wassssssuppppppp@users.noreply.github.com>
2022-11-24 18:38:26 +0300

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EfCoreTest1
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeCode { get; set; }
+
+        public string FIOemployee { get; set; }
+
+        public int PositionCode { get; set; }
+
+    }
+}
---
+++ StoreAddress.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EfCoreTest1
+{
+    public class StoreAddress
+    {
+        [Key]
+        public int Store_addres_code { get; set; }
+
+        public string Store_address { get; set; }
+
+    }
+}
